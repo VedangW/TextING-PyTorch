ccdca78f3373b198f6142811ea24ac69f547456e
Vedang Waradpande <vedang.waradpande@gmail.com>
2022-04-19 17:19:33 -0400

    Add GraphLayer

--- texting/layers.py
+++ texting/layers.py
@@ -1,3 +1,4 @@
+from turtle import forward
 import torch
 import torch.nn as nn
 from torch.nn.parameter import Parameter, UninitializedParameter
@@ -68,6 +69,9 @@ def dot(x, y, sparse=False):
 
     return res
 
+def gru_unit(support, x, var, act, mask, dropout, sparse_inputs=False):
+    return x
+
 
 class Dense(nn.Module):
     """
@@ -106,3 +110,63 @@ class Dense(nn.Module):
             output += self.bias
 
         return self.activation(output)
+
+
+class GraphLayer(nn.Module):
+
+    def __init__(self, input_dim, output_dim, configs, dropout=False,
+                 sparse_inputs=False, activation=nn.ReLU, bias=False,
+                 featureless=False, steps=2, **kwargs):
+        """ Note: dropout value is passed from config, and not dropout. """
+
+        super(GraphLayer, self).__init__(**kwargs)
+
+        if dropout:
+            self.dropout = configs['dropout']
+        else:
+            self.dropout = 0.
+        
+        self.activation = activation
+        self.support = configs['support']
+        self.sparse_inputs = sparse_inputs
+        self.featureless = featureless
+        self.bias = bias
+        self.mask = configs['mask']
+        self.steps = steps
+
+        # helper variable for sparse dropout
+        self.num_features_nonzero = configs['num_features_nonzero']
+
+        self.vars = nn.ParameterDict({
+            'weights_encode': Parameter(glorot([input_dim, output_dim])),
+            'weights_z0': Parameter(glorot([output_dim, output_dim])),
+            'weights_z1': Parameter(glorot([output_dim, output_dim])),
+            'weights_r0': Parameter(glorot([output_dim, output_dim])),
+            'weights_r1': Parameter(glorot([output_dim, output_dim])),
+            'weights_h0': Parameter(glorot([output_dim, output_dim])),
+            'weights_h1': Parameter(glorot([output_dim, output_dim])),
+            'bias_encode': Parameter(zeros([output_dim])),
+            'bias_z0': Parameter(zeros([output_dim])),
+            'bias_z1': Parameter(zeros([output_dim])),
+            'bias_r0': Parameter(zeros([output_dim])),
+            'bias_r1': Parameter(zeros([output_dim])),
+            'bias_h0': Parameter(zeros([output_dim])),
+            'bias_h1': Parameter(zeros([output_dim]))
+        })
+
+    def forward(self, x):
+        if self.sparse_inputs:
+            x = sparse_dropout(x, 1-self.dropout, self.num_features_nonzero)
+        else:
+            dropout = nn.Dropout(p=1-self.dropout)
+            x = dropout(x)
+
+        x = dot(x, self.vars['weights_encode'], self.sparse_inputs) + self.vars['bias_encode']
+        output = self.mask * self.activation(x)
+
+        for _  in range(self.steps):
+            output = gru_unit(self.support, output, self.vars, 
+                              self.activation, self.mask, 1-self.dropout, 
+                              self.sparse_inputs)
+
+        return output
