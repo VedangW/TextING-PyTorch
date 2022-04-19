da1e1988b36fde76bebfe5528b90a7c9ed5d22f6
Vedang Waradpande <vedang.waradpande@gmail.com>
2022-04-19 15:14:48 -0400

    Setup file for linux based systems

---
+++ texting_setup.sh
@@ -0,0 +1,8 @@
+conda create -n "texting" python=3.6
+conda activate texting
+conda install -c anaconda scipy=1.5.2 -y
+conda install pytorch torchvision -c pytorch -y
+conda install tensorflow-gpu=1.12.0 -y
+conda install scikit-learn -y
+conda install nltk -y
+conda install tqdm -y
