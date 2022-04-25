a9a8c4204e4c69fe0a32d9b34e6df0059f29180a
Vedang Waradpande <vedang.waradpande@gmail.com>
2022-04-25 17:16:18 -0400

    Add build_graph and remove_words

--- texting/build_graph.sh
+++ texting/build_graph.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#SBATCH --partition=gpu           # Partition (job queue)
+#SBATCH --partition=main          # Partition (job queue)
 #SBATCH --requeue                 # Return job to the queue if preempted
-#SBATCH --job-name=texting        # Assign an short name to your job
+#SBATCH --job-name=textinggraph   # Assign an short name to your job
 #SBATCH --cpus-per-task=1         # Cores per task (>1 if multithread tasks)
 #SBATCH --mem-per-cpu=16000M      # Real memory (RAM) required
-#SBATCH --gres=gpu:2              # Generic resources
 #SBATCH --time=02:00:00           # Total run time limit (HH:MM:SS)
 #SBATCH --error=/scratch/vw120/texting/slurm.texting.%N.%j.err
 #SBATCH --out=/scratch/vw120/texting/slurm.texting.%N.%j.out
@@ -15,4 +14,4 @@
 source /home/vw120/miniconda3/bin/activate
 conda activate texting
 
-python /home/vw120/TextING-PyTorch/texting/train.py
+python /home/vw120/TextING-PyTorch/texting/build_graph.py
